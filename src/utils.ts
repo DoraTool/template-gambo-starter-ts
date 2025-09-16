928e61ed21380d7c794ac893f29392f7a7f9dd14
zhaolei <zhaolei@dora.design>
2025-09-16 11:49:34 +0800

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -21,3 +21,17 @@ export const createTrigger = (
     (customCollider.body as Phaser.Physics.Arcade.Body).setImmovable(true);
     return customCollider;
 };
+
+
+/**
+ * 计算素材旋转角度（upAngle 和 flightAngle 都按顺时针计算）
+ * @param {number} upAngle - 素材自身朝向，顺时针角度
+ * @param {number} targetAngle - 期望飞行方向，顺时针角度
+ * @returns {number} 最终旋转角度（弧度）
+ * 以素材箭头朝向右上角，角度为45度，飞行角度为屏幕空间垂直向上飞行角度0度计算示例
+ * this.sprite.rotation = computeRotationCW(45, 0);
+ */
+export function computeRotationCW(upAngle: number, targetAngle: number) {
+  // 两者都按顺时针，直接相减即可
+  return Phaser.Math.DegToRad(targetAngle - upAngle);
+}
