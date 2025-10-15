e97f0049b8562307848ece7cdcbdc86dadc61197
easyshow-official <official@easyshow.ai>
2025-10-15 17:25:13 +0800

    add UIScene

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,9 +1,6 @@
 import Phaser from "phaser"
 
 export default class GameScene extends Phaser.Scene {
-    preload() {
-        // Load assets here
-    }
 
     create() {
         // Create game objects here
